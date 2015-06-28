a6c314bd343a17e07b04cad067f1a1d9b9d0ce8d
Paul D <pdaschak@gmail.com>
2015-06-28 22:28:07 +0300

    Initial Commit

---
+++ BudsiesDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BudsiesDemoApp
+//
+//  Created by Paul D on 28.06.15.
+//  Copyright (c) 2015 Paul D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

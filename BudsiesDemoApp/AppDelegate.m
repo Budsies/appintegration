0a11178b04ce57833dd99468f717727554750a56
Paul D <pdaschak@gmail.com>
2016-01-29 00:05:52 +0300

    add save and restore cookies

--- BudsiesDemoApp/AppDelegate.m
+++ BudsiesDemoApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "NSHTTPCookieStorage+Additional.h"
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
 
@@ -16,11 +17,26 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Fabric with:@[CrashlyticsKit]];
 
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] load];
+
     return YES;
+}
+
+// this 3 methods needs to save/restore cookies, because they are not saves on the next launch the app and
+// if app in background mode
+- (void)applicationWillTerminate:(UIApplication *)application {
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] save];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] load];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[NSHTTPCookieStorage sharedHTTPCookieStorage] save];
 }
 
 
---
+++ BudsiesDemoApp/Categories/NSHTTPCookieStorage+Additional.h
@@ -0,0 +1,16 @@
+//
+//  NSHTTPCookieStorage+Additional.h
+//  BudsiesDemoApp
+//
+//  Created by Paul D on 28.01.16.
+//  Copyright © 2016 Paul D. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSHTTPCookieStorage (Additional)
+
+- (void)save;
+- (void)load;
+
+@end
